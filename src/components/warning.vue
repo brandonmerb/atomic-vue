917625df4dc855e605f34548eb2908e394f66c8d
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-15 23:48:36 -0400

    Super simple warning component changes

--- src/components/warning.vue
+++ src/components/warning.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    If you're seeing this message, that means you haven't installed any
-    modules that contain a master template
+  <div class="container">
+    <div class="card">
+      <div class="card-title">
+        <h2>Atomic Vue</h2>
+      </div>
+      <div class="card-text">
+        If you're seeing this message, that means you haven't installed any
+        modules that contain a master template
+      </div>
+    </div>
   </div>
 </template>
+<style>
+html, body {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
+<style scoped>
+.container {
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+
+  justify-content: center;
+  align-content: center;
+}
+.card {
+  border: 1px solid #CCC;
+  border-radius: 5px;
+}
+.card-title{
+}
+.card-text {
+}
+</style>
